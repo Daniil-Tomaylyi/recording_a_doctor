9a696e5089898a5931cd83ede5ce8388e79c7f39
Daniil <daniiltomajly@gmail.com>
2022-12-21 15:02:42 +0300

    Auto build APK file

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 
 
-class MainApp(MDApp):
+class Recordingdoctor(MDApp):
     def build(self):
         return MDLabel(text="Hello, World", halign="center")
 
 
-MainApp().run()
+Recordingdoctor().run()
